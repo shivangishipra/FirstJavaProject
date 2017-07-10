4f4c86546bb026ccc504ba05f9105ffa51a44846
Shivangi Shipra <shivangi.shipra@accoliteindia.com>
2017-07-10 18:24:18 +0530

    first commit

---
+++ src/first/firstClass.java
@@ -0,0 +1,26 @@
+package first;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class firstClass.
+ */
+public class firstClass {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Sonal";
+		System.out.println("Hello World!! " + name);
+		System.out.println("Hey checking break point.");
+		int val = 0;
+		for (int i = 0; i < 5; i++) {
+			val++;
+		}
+		System.out.println("val : " + val);
+	}
+
+}
